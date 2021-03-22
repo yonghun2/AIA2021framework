891541503890df727b1f49ee2555cece9cc86344
bit01class <bit01class@gmail.com>
2021-03-22 17:47:06 +0900

    spring 3.x & ibatis

--- sts02/src/main/java/com/bit/sts02/controller/EmpController.java
+++ sts02/src/main/java/com/bit/sts02/controller/EmpController.java
@@ -3,9 +3,12 @@ package com.bit.sts02.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.bit.sts02.model.DeptDao;
 import com.bit.sts02.model.EmpDao;
@@ -35,6 +38,23 @@ public class EmpController {
 		return "redirect:list";
 	}
 	
+	@RequestMapping(value = "/one", method = RequestMethod.GET)
+	public void detail(@RequestParam("idx") int empno,Model model) {
+		model.addAttribute("bean", empDao.selectOne(empno));
+		model.addAttribute("list", deptDao.selectAll());
+	}
+	
+	@RequestMapping(value = "/one", method = RequestMethod.POST)
+	public String update(@ModelAttribute EmpVo bean) {
+		empDao.updateOne(bean);
+		return "redirect:./one?idx="+bean.getEmpno();
+	}
+	
+	@RequestMapping(value = "/one/{empno}",method = RequestMethod.POST)
+	public String delete(@PathVariable("empno") int empno) {
+		empDao.deleteOne(empno);
+		return "redirect:../list";
+	}
 }
 
 
